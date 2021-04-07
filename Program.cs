13a4e3f8f25d2c284c25d1f69223867fbf4e9fb9
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-04-07 11:34:31 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Definiciones
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+           const int puertas = 20, ventanas = 10;
+           int casa = 2, t_puertas, t_ventanas;
+
+           t_puertas = puertas * casa;
+           t_ventanas = ventanas * casa;
+
+           Console.WriteLine("Puertas: {0} Ventanas: {1}", t_puertas, t_ventanas);
+
+           if(t_puertas < t_ventanas){
+               Console.WriteLine("Hay mas ventanas que puertas en la casa");
+           }else if(t_puertas > t_ventanas){
+               Console.WriteLine("Hay mas puertas que ventanas en la casa");
+           }else{
+               Console.WriteLine("Hay el mismo numero de puertas y ventanas");
+           }
+        }
+    }
+}
