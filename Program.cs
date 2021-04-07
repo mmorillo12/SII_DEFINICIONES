88e7e4dffe1cf3e552ad86926a10d451d651dfae
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-04-07 10:59:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Definiciones
+{
+    class Ventana{
+        int ancho;
+        int alto;
+        string lugar;
+        bool estado;
+        
+        public Ventana(int alto, string lugar, bool estado){
+            this.alto = alto;
+            this.lugar = lugar;
+            this.estado = estado;
+        }
+        ~Ventana(){
+            //Liberar y Cerrar
+        }
+        public void Mostrar(){
+            Console.WriteLine("El alto de la ventana es {0}", alto);
+            Console.WriteLine("La ventana esta en la {0}", lugar);
+            Console.WriteLine("La ventana esta abierta?: {0}", estado);
+        }
+    }
+    
+class Program
+    {
+        static void Main(string[] args)
+        {
+           Ventana v = new Ventana(20, "Sala", true);
+           v.Mostrar();
+        }
+    }
+}
