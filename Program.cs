1dcf9f7992d38780d4572a942f975c467fdfe682
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-04-07 11:21:58 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Definiciones
+{
+    class Triangulo{
+        double Base;
+        double altura;
+
+        public double Bases{get => Base; set => Base = value;}
+        public double Altura{get => altura; set => altura = value;}
+       public double Area(){
+           return (Base*Altura)/2;
+       }
+}
+    
+class Program
+    {
+        static void Main(string[] args)
+        {
+           Triangulo t = new Triangulo();
+           t.Altura = 13;
+           t.Bases = 18;
+           Console.WriteLine("El Area del triangulo es {0}",t.Area());
+        }
+    }
+}
