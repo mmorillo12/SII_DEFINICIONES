69d12180cf74926c252129ff794e556d3b4437bb
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-04-07 10:13:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Definiciones
+{
+    public class ClaseA{
+    public ClaseA(){
+        Console.WriteLine("Que bello es vivir");
+    }
+}
+public class ClaseB : ClaseA{
+    public ClaseB(){
+        Console.WriteLine("La vida es corta aprovechala");
+    }
+}
+public class ClaseC : ClaseB{
+    public ClaseC(){
+        Console.WriteLine("Aprovechemos el tiempo");
+    }
+}
+class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseC c = new ClaseC();
+        }
+    }
+}
